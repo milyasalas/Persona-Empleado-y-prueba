64bbf14d5653a2a4ac6a2c60c915dff6185f4d4e
milyasalas <117372618+milyasalas@users.noreply.github.com>
2022-12-13 10:29:35 -0400

    Add files via upload

---
+++ Empleado.java
@@ -0,0 +1,12 @@
+
+public class Empleado extends Persona {
+    protected int sueldo;
+    public void cargarSueldo() {
+        System.out.print("Ingrese su sueldo:");
+        sueldo=teclado.nextInt();
+    }
+
+    public void imprimirSueldo() {
+        System.out.println("El sueldo es:"+sueldo);
+    }
+}
---
+++ Prueba.java
@@ -0,0 +1,12 @@
+public class Prueba {
+    public static void main(String[] ar) {
+        Persona persona1 = new Persona();
+        persona1.cargarDatosPersonales();
+        Empleado empleado1 = new Empleado();
+        empleado1.cargarDatosPersonales();
+        empleado1.cargarSueldo();
+        persona1.imprimirDatosPersonales();
+        empleado1.imprimirDatosPersonales();
+        empleado1.imprimirSueldo();
+    }
+}
